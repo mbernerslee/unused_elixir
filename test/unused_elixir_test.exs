50a2d76910ef769db0f5aa79ae457cdccc990cb3
Berners <matthewbernerslee@gmail.com>
2018-06-22 17:14:53 +0100

    adding a todo

--- test/unused_elixir_test.exs
+++ test/unused_elixir_test.exs
@@ -2,6 +2,8 @@ defmodule UnusedElixirTest do
   use ExUnit.Case
   doctest UnusedElixir
 
+  #TODO Make it a mix task!
+
   test "greets the world" do
     assert UnusedElixir.hello() == :world
   end
